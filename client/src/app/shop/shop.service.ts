ebf125f1f8ba73aab4ed2bfbf615811484343718
nod-codeman <nod.codeman@gmail.com>
2020-10-02 22:22:44 -0400

    Added filter and sort functionality

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IBrand } from '../shared/Models/brands';
 import { IPagination } from '../shared/Models/pagination';
 import { IType } from '../shared/Models/productType';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,26 @@ export class ShopService {
   constructor(private http: HttpClient  ) { }
 
   // method to return products from the api
-  getProducts(){
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number, sort?: string){
+    let params = new HttpParams();
+
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append('brandId', typeId.toString());
+    }
+
+    if (sort) {
+      params = params.append('sort', sort);
+    }
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+    .pipe( // use pipe to extract the http object
+      map(response => {
+        return response.body;
+      })
+    )
   }
 
   getBrands(){
